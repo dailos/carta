684830115a39fb79e2f1e117f3b2c6b88fe4df35
Dailos Marrero <dailos.marrero@westwing.de>
2019-12-30 01:16:42 +0000

    new design

---
+++ resources/views/layouts/partials/menu.blade.php
@@ -0,0 +1,51 @@
+<header class="menu clearfix">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+            <a class="navbar-brand" href="http://fedac.org/">
+                <img  src=" {{asset('images/menu_logo.png')}} " alt="Carta Etnográfica">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto navbar-nav-left">
+                    <li class="nav-item">
+                        <a href="https://fedac.org" class="nav-link">FEDAC</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://fedac.org/actividades" class="nav-link">Actividades</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://fedac.org/artesanos" class="nav-link">Artesanos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://culturatradicionalgc.org" class="nav-link">Cultura Tradicional</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://fedac.org/fotos-antiguas" class="nav-link">Fotos antiguas</a>
+                    </li>
+                    <li class="nav-item dropdown active">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Carta etnográfica
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="https://cartaetnograficagc.org/">Presentación</a>
+                            <a class="dropdown-item" href="https://cartaetnograficagc.org/la-carta-etnografica/">La carta etnográfica</a>
+                            <a class="dropdown-item" href="https://cartaetnograficagc.org/historia/">Historia del proyecto</a>
+                            <a class="dropdown-item" href="https://cartaetnograficagc.org/sobre-las-fichas/">Sobre las fichas</a>
+                            <a class="dropdown-item" href="https://cartaetnograficagc.org/tipos-de-bienes/">Tipos de bienes</a>
+                            <a class="dropdown-item" href="https://cartaetnograficagc.org/participantes/">Participantes</a>
+                            <a class="dropdown-item active" href="/">Acceso a las fichas</a>
+                            <a class="dropdown-item" href="https://cartaetnograficagc.org/rutas/">Rutas etnográficas</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://fedac.org/fondo-etnografico/" class="nav-link">Fondo etnográfico</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -12,7 +12,6 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
     <!-- Styles -->
@@ -24,26 +23,12 @@
 </head>
 <body>
 
-<div class="ffb-id-navigation-header wrapper ff-boxed-wrapper">
-    <div class="wrapper-top-space"></div>
-    @include('layouts.partials.header')
+    @include('layouts.partials.menu')
+
     <div class="page-wrapper">
-
-        @include('layouts.partials.main_image')
-        @include('layouts.partials.submenu')
-
-        <section class="ffb-id-23a5jmvo fg-section fg-text-dark">
-            <div class="fg-container container fg-container-large fg-container-lvl--1 ">
-                <div class="fg-row row    ">
-                    <div class="ffb-id-23a5jmvp fg-col col-xs-12 col-sm-12 col-md-4 fg-el-has-bg fg-text-dark"><span class="fg-bg"><span data-fg-bg="{&quot;type&quot;:&quot;color&quot;,&quot;opacity&quot;:1,&quot;color&quot;:&quot;#086c00&quot;}" class="fg-bg-layer fg-bg-type-color " style="opacity: 1; background-color: #086c00;"></span></span>
-                        <h1 class="ffb-id-23a5jmvr fg-heading text-center    fg-text-dark"><p><span style="background-color: #086c00;color: #ffffff">ACCESO A LAS FICHAS<br/></span></p></h1>
-                    </div>
-                </div>
-            </div>
-        </section>
         <section class="ffb-id-2h51fn7v fg-section fg-text-dark">
             <div class="fg-container container fg-container-large fg-container-lvl--1 ">
-                <div class="fg-row row    ">
+                <div class="fg-row row">
                     <div class="ffb-id-2n0ocp1j fg-col col-xs-12 col-md-12 fg-text-dark">
                         <div class="ffb-id-2n0ocson embed-video-external">
                             <div class="embed-responsive">
@@ -59,13 +44,6 @@
 
         @include('layouts.partials.footer')
     </div>
-</div>
-<a href="javascript:void(0);" class="js-back-to-top back-to-top-theme"></a>
-<div
-    class="hidden smoothscroll-sharplink"
-    data-speed="1000"
-    data-offset="0"
-></div>
 
 <!-- Scripts -->
 <script src="{{ asset('js/manifest.js') }}"></script>
